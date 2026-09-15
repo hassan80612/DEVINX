3e3657aef7f567486d55fec97d45bf2ec7de5535
hassan80612 <hassan80612@gmail.com>
2026-09-15 14:56:08 -0300

    feat: add categories page

---
+++ src/app/categorias/page.tsx
@@ -0,0 +1,6 @@
+import {AppShell} from '@/components/AppShell';
+import {CategoryManager} from '@/components/CategoryManager';
+
+export default function CategoriasPage(){
+  return <AppShell titleKey="categoriesTitle"><CategoryManager/></AppShell>;
+}
