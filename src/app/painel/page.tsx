3c92bf75e4ed68ea6b62943c0578c841df6a16ff
hassan80612 <hassan80612@gmail.com>
2026-09-15 11:34:22 -0300

    i18n: use dashboard title key

--- src/app/painel/page.tsx
+++ src/app/painel/page.tsx
@@ -1,4 +1,1 @@
-import {AppShell} from '@/components/AppShell';
-import {DashboardOverview} from '@/components/DashboardOverview';
-
-export default function Painel(){return <AppShell title="Seu mês"><DashboardOverview/></AppShell>}
+import {AppShell} from '@/components/AppShell';import {DashboardOverview} from '@/components/DashboardOverview';export default function Painel(){return <AppShell titleKey="dashboardTitle"><DashboardOverview/></AppShell>}
