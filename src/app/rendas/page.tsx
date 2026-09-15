aeaaf1f83ed262c7a880411d91e396387641140a
hassan80612 <hassan80612@gmail.com>
2026-09-15 11:34:36 -0300

    i18n: centralize income page copy

--- src/app/rendas/page.tsx
+++ src/app/rendas/page.tsx
@@ -1,4 +1,2 @@
-import {AppShell} from '@/components/AppShell';
-import {TransactionManager} from '@/components/TransactionManager';
-
-export default function Rendas(){return <AppShell title="Rendas"><p className="lead">Tudo o que entra, separado por origem.</p><TransactionManager kind="income"/></AppShell>}
+'use client';
+import {AppShell} from '@/components/AppShell';import {TransactionManager} from '@/components/TransactionManager';import {useI18n} from '@/i18n/provider';export default function Rendas(){const{messages:m}=useI18n();return <AppShell titleKey="incomeTitle"><p className="lead">{m.pages.incomeLead}</p><TransactionManager kind="income"/></AppShell>}
