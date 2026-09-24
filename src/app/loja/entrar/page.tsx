ef7943dd1efafa051367f0a60333dee34c3a275c
hassan80612 <hassan80612@gmail.com>
2026-09-24 00:53:31 -0300

    fix: identificar explicitamente o login da DevinX Loja

--- src/app/loja/entrar/page.tsx
+++ src/app/loja/entrar/page.tsx
@@ -1,5 +1,5 @@
 import {redirect} from "next/navigation";
 
 export default function StoreLoginBridge(){
-  redirect("https://www.vetorizeai.com.br/login?next=%2Fminha-loja%2Fpainel");
+  redirect("https://www.vetorizeai.com.br/login?context=store&next=%2Fminha-loja%2Fpainel");
 }
