6b18f32b99bc8c46092fa8a640c12f7837390889
hassan80612 <hassan80612@gmail.com>
2026-09-24 01:00:52 -0300

    Usar rota dedicada de login da DevinX Loja
    O bridge da Loja passa a usar a rota dedicada de autenticação, evitando o login genérico do VetorizeAI.

--- src/app/loja/entrar/page.tsx
+++ src/app/loja/entrar/page.tsx
@@ -1,5 +1,5 @@
 import {redirect} from "next/navigation";
 
 export default function StoreLoginBridge(){
-  redirect("https://www.vetorizeai.com.br/login?context=store&next=%2Fminha-loja%2Fpainel");
+  redirect("https://www.vetorizeai.com.br/minha-loja/entrar?next=%2Fminha-loja%2Fpainel");
 }
