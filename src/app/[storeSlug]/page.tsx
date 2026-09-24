38f636d5690f1f1ae4a2352b4e5e3053b7460478
hassan80612 <hassan80612@gmail.com>
2026-09-24 18:53:07 -0300

    Usar cache curto na vitrine pública

--- src/app/[storeSlug]/page.tsx
+++ src/app/[storeSlug]/page.tsx
@@ -4,8 +4,7 @@ import {fetchPublicStorefront} from "@/lib/storefront-backend";
 // @ts-ignore -- Storefront client remains in JavaScript during the staged migration.
 import StorefrontClient from "./StorefrontClient";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const revalidate = 30;
 
 type RouteProps={params:Promise<{storeSlug:string}>};
 
