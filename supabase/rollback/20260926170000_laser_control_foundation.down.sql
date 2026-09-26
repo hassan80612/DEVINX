895f84b4f7bef45ad0272ceb49db0b4315e9bfd2
hassan80612 <hassan80612@gmail.com>
2026-09-26 14:39:40 -0300

    Remover rollback provisório substituído pela versão aplicada

--- supabase/rollback/20260926170000_laser_control_foundation.down.sql
+++
@@ -1,5 +0,0 @@
--- Rollback companion for 20260926170000_laser_control_foundation.sql
--- Run ONLY if the corresponding Laser Control migration was actually applied.
--- Current foundation stores all Laser Control database objects in this private schema.
-
-drop schema if exists devinx_laser cascade;
