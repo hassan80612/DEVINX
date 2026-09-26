5389ec77c4ae0ef46f8a15e18094991ae4ad6a22
hassan80612 <hassan80612@gmail.com>
2026-09-26 18:10:10 -0300

    Atualizar Agent para versão 0.2.0

--- laser-agent/PairingProofFactory.cs
+++ laser-agent/PairingProofFactory.cs
@@ -15,7 +15,7 @@ internal sealed record PairingProof(
 
 internal static class PairingProofFactory
 {
-    public const string AgentVersion = "0.1.0";
+    public const string AgentVersion = "0.2.0";
     private static readonly char[] Alphabet = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789".ToCharArray();
 
     public static PairingProof Create(TimeSpan lifetime)
